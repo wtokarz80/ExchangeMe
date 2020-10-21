b322da1e620318951a82663722f1b5c21722ff7b
wtokarz80 <w.tokarz80gmail.com>
2020-10-21 11:35:56 +0200

    fixed pom configurations

--- src/main/java/com/exchangeme/servlets/UserServlet.java
+++ src/main/java/com/exchangeme/servlets/UserServlet.java
@@ -9,8 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 @WebServlet(name = "UserServlet", urlPatterns = {"/users"}, loadOnStartup = 1)
 
@@ -28,29 +27,5 @@ public class UserServlet extends HttpServlet {
 
 
 
-    public List<User> getAll() {
-
-        EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
-                .createEntityManagerFactory("exchangeMe");
-
-        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
-        String strQuery = "SELECT u FROM User u WHERE u.id IS NOT NULL";
-
-        // Issue the query and get a matching Customer
-        TypedQuery<User> tq = em.createQuery(strQuery, User.class);
-        List<User> users = new ArrayList<>();
-        try {
-            // Get matching customer object and output
-            users = tq.getResultList();
-            users.forEach(user->System.out.println(user.getUserName() + " " + user.getEmail() + " " + user.getPassword()));
-        }
-        catch(NoResultException ex) {
-            ex.printStackTrace();
-        }
-        finally {
-            em.close();
-        }
-        return users;
-    }
 
 }
