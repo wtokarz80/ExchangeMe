fbaff21e210e4daf23e7232ff1323615d2832dc8
wtokarz80 <w.tokarz80gmail.com>
2020-10-22 16:38:51 +0200

    implement methods doPost and doDelete in userServlet

--- src/main/java/com/exchangeme/servlets/UserServlet.java
+++ src/main/java/com/exchangeme/servlets/UserServlet.java
@@ -2,6 +2,7 @@ package com.exchangeme.servlets;
 
 import com.exchangeme.dao.UserDao;
 import com.exchangeme.models.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.persistence.*;
@@ -13,6 +14,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static com.exchangeme.helpers.DataParser.parseQueryString;
 
 
 @WebServlet(name = "UserServlet", urlPatterns = {"/users", "/users/*"}, loadOnStartup = 1)
@@ -29,49 +34,66 @@ public class UserServlet extends HttpServlet {
         String[] splitURI = request.getRequestURI().split("/");
         if(splitURI.length == 2){
             List<User> users = userDao.getAll();
-            System.out.println(users);
             out.println(objectMapper.writeValueAsString(users));
 
         } else if (splitURI.length == 3 && splitURI[2].matches("\\d+")) {
-            long userId = Long.parseLong(splitURI[2]);
-            User user = userDao.getUser(userId);
-            System.out.println(user);
-            out.println(objectMapper.writeValueAsString(user));
+            getOneUser(splitURI, out);
         }
         else {
-            out.println(
-                    "<html>\n" +
-                            "<head><title>USERS</title></head>\n" +
-                            "<body>\n" +
-                            "<h2 align = \"center\"><< SOMETHING GOES WRONG >></h2>\n" +
-                            "</body></html>"
-            );
+            String errorMessage = "SOMETHING GOES WRONG";
+            out.println(errorMessage);
         }
 
 
 
+    }
+
+    private void getOneUser(String[] splitURI, PrintWriter out) {
+        long userId = Long.parseLong(splitURI[2]);
+        Optional<User> user = userDao.getById(userId);
+        user.ifPresentOrElse(
+                u -> {
+                    try {
+                        out.println(objectMapper.writeValueAsString(u));
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                    }
+                },
+                () -> out.println("There is no user with the given id")
+        );
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        User user = new User();
+        String queryString = request.getQueryString();
+        Map<String,String> parsedQuery = parseQueryString(queryString);
+        if(parsedQuery.containsKey("name")
+                && parsedQuery.containsKey("email")
+                && parsedQuery.containsKey("password"))
+        {
+            user.setUserName(parsedQuery.get("name"));
+            user.setEmail(parsedQuery.get("email"));
+            user.setPassword(parsedQuery.get("password"));
+            userDao.insert(user);
+        } else {
+            response.getWriter().println("SOMETHING GOES WRONG");
+        }
+    }
+
+
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
 
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        PrintWriter out = response.getWriter();
-//        String title = "TEST SERVLET";
-//
-//        out.println(
-//                "<html>\n" +
-//                        "<head><title>" + title + "</title></head>\n" +
-//                        "<body>\n" +
-//                        "<h1 align = \"center\">" + title + "</h1>\n" +
-//                        "<h2 align = \"center\"><< HELLO USERS >></h2>\n" +
-//                        "</body></html>"
-//        );
-//    }
-
-
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String[] splitURI = request.getRequestURI().split("/");
+        if (!(splitURI.length == 3 && splitURI[2].matches("\\d+"))) {
+            response.getWriter().println("SOMETHING GOES WRONG");
+        }
+        long userId = Long.parseLong(splitURI[2]);
+        userDao.delete(userId);
+    }
 
 
 }
