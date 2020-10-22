cfcb16cff9772f9345a8edbf0f079bf3d34f7255
Aldona <aldonapawlak2@gmail.com>
2020-10-22 03:26:24 +0200

    Handle adding item to DB

--- src/main/java/com/exchangeme/dao/ItemDao.java
+++ src/main/java/com/exchangeme/dao/ItemDao.java
@@ -1,7 +1,9 @@
 package com.exchangeme.dao;
 
-import com.exchangeme.models.Item;
+import com.exchangeme.models.*;
+
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.ArrayList;
@@ -10,8 +12,10 @@ import static com.exchangeme.helpers.EntityM.ENTITY_MANAGER_FACTORY;
 
 public class ItemDao {
 
+    EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+    EntityTransaction transaction = em.getTransaction();
+
     public List<Item> getAll() {
-        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
         String strQuery = "SELECT i FROM Item i WHERE i.id IS NOT NULL";
         TypedQuery<Item> tq = em.createQuery(strQuery, Item.class);
         List<Item> items = new ArrayList<>();
@@ -28,22 +32,39 @@ public class ItemDao {
         return items;
     }
 
-    public Item getItem(long id) {
-        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
-        String query = "SELECT i FROM Item i WHERE i.id = :id";
-        TypedQuery<Item> tq = em.createQuery(query, Item.class);
-        tq.setParameter("id", id);
-        Item item = null;
-        try {
-            item = tq.getSingleResult();
-            return item;
-        }
-        catch(NoResultException ex) {
-            ex.printStackTrace();
-        }
-        finally {
-            em.close();
-        }
-        return item;
+    public Item getItemById(long id) {
+        return em.find(Item.class, id);
     }
+
+    public void addItem(long id, String name, Category category, Location location, Status status, String description) {
+        User user = em.find(User.class, id);
+        if (id == user.getId()) {
+            Item item = new Item(user, name, category, location, status, description);
+            transaction.begin();
+            em.persist(item);
+            transaction.commit();
+        } else {
+            System.out.println("No such user id in DB");
+        }
+    }
+
+//
+//    public Item getItem(long id) {
+//        EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+//        String query = "SELECT i FROM Item i WHERE i.id = :id";
+//        TypedQuery<Item> tq = em.createQuery(query, Item.class);
+//        tq.setParameter("id", id);
+//        Item item = null;
+//        try {
+//            item = tq.getSingleResult();
+//            return item;
+//        }
+//        catch(NoResultException ex) {
+//            ex.printStackTrace();
+//        }
+//        finally {
+//            em.close();
+//        }
+//        return item;
+//    }
 }
